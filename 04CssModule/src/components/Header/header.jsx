53685b3056fbcf501b34ea684641d79e590f1c18
urmit sardhara <sardharaurmit@gmail.com>
2026-05-02 22:38:05 +0530

    add new tailwin

---
+++ 04CssModule/src/components/Header/header.jsx
@@ -0,0 +1,13 @@
+import style from"./Header.module.css"
+function Header() {
+  return (
+    <header className={style.head}>
+        <h1>wellcome </h1>
+       
+        <button>click</button>
+      
+    </header>
+  )
+}
+
+export default Header
