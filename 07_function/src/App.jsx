cbc2622cc3b759979ebb33e2298ada7ae97bd96a
urmit sardhara <sardharaurmit@gmail.com>
2026-05-07 10:40:33 +0530

    add new funtion with events

---
+++ 07_function/src/App.jsx
@@ -0,0 +1,41 @@
+
+function App() {
+  // function click(){
+  //   console.log("btn is clicked");
+    
+  // }
+  // function dblclk (){
+  //   console.log("btn is click two time ");
+    
+  // }
+  // const onM =()=>{
+  //   console.log("on mouse");
+    
+  // }
+  return (
+    <div>
+      <h1>hello</h1>
+      {/* <button onClick={click} onDoubleClick={dblclk} onMouseOver={onM}>clcick</button>
+
+
+      <button onClick={function clk(){
+        console.log("this is click by user fun ");
+        
+      }}>in build fun</button>
+
+
+
+    <input onChange={()=>{
+      console.log("th user is typing ");
+      
+    }} type="text" placeholder="enter name " /> */}
+
+
+
+
+    
+    </div>
+  )
+}
+
+export default App
