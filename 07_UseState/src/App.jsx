ed8bb71790a5150e9d0c8fb72b17d9e094d5e45a
urmit sardhara <sardharaurmit@gmail.com>
2026-05-08 18:44:20 +0530

    add new arr with usestate

--- 07_UseState/src/App.jsx
+++ 07_UseState/src/App.jsx
@@ -5,16 +5,23 @@ function App() {
 
 const [num, setnum] = useState(20)
 const [name, setname] = useState("urmit")
+let [arr, setarr] = useState([10,20,30])
 
 function change (){
   setnum(30)
   setname("king")
+  setarr([20,40,60])
+  console.log(typeof(arr));
+  
   
   
 }
   return (
     <div>
-    <h1>num is the {num} <br />the name is the {name}</h1> 
+    <h1>num is the {num} <br />
+    the name is the {name} <br />
+    the arr is the {arr}
+    </h1> 
     <button onClick={change}>click</button>     
     </div>
   )
