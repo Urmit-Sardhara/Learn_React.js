f1401a5983e496ffbd05c7b0b23e3a0135fa2fed
urmit sardhara <sardharaurmit@gmail.com>
2026-05-08 18:38:07 +0530

    add new name usestate

--- 07_UseState/src/App.jsx
+++ 07_UseState/src/App.jsx
@@ -4,16 +4,17 @@ import { useState } from "react"
 function App() {
 
 const [num, setnum] = useState(20)
+const [name, setname] = useState("urmit")
 
 function change (){
-  console.log(num);
   setnum(30)
+  setname("king")
   
   
 }
   return (
     <div>
-    <h1>num is the {num}</h1> 
+    <h1>num is the {num} <br />the name is the {name}</h1> 
     <button onClick={change}>click</button>     
     </div>
   )
