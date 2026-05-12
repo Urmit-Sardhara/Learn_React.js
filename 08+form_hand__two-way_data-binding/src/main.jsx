3695438343679d9418ad15f50f7736b97ba44eca
urmit sardhara <sardharaurmit@gmail.com>
2026-05-12 11:49:36 +0530

    add ne two way data binding

--- 08+form_hand__two-way_data-binding/src/main.jsx
+++ 08+form_hand__two-way_data-binding/src/main.jsx
@@ -1,9 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// import Form_handler from './form_handler'
 
 createRoot(document.getElementById('root')).render(
-  
+    // <Form_handler/>
     <App />
  
 )
